34735f7934f67111bcd53b349351fbd7aeeb76ab
Jim Farrugia <the.jim.farrugia@gmail.com>
2018-12-31 20:54:04 +1100

    Added validation, cleaned up script, tweaked HTML

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-// * EVENT LISTENERS *
-// Listen for 'CLICK' on calculate-btn
+// Listen for click on Calculate-Btn
 document.getElementById("calculate-btn").addEventListener("click", function() {
-  // * GET USER INPUT *
+  // Get user input
   const baseInput = document.getElementById("base-input").value;
   const targetInput = document.getElementById("target-input").value;
   const amountInput = document.getElementById("amount-input").value;
   const outputArea = document.getElementById("result");
-  // Check for valid inputs
-  if (!baseInput && !targetInput && !amountInput) {
-    // are they truthy values?
-    console.log("no");
-  } else {
-    console.log("yes");
-  }
-  // Call calculate function
-  calculateNicToAdd(baseInput, targetInput, amountInput, outputArea);
-  // Display result
+  // Run calculateNicToAdd and display return value.
+  outputArea.innerHTML = calculateNicToAdd(baseInput, targetInput, amountInput);
 });
 
 // CALCULATE
-function calculateNicToAdd(base, target, amount, outputArea) {
-  // check for valid inputs
+function calculateNicToAdd(base, target, amount) {
+  // Check for invalid inputs
+  if (!base || !target || !amount) {
+    // values are not truthy
+    return `<p>One or more input is invalid.</p>`;
+  }
+  if (base == 0 || target == 0 || amount == 0) {
+    // values cannot be 0
+    return `<p>All inputs must be grater than zero.</p>`;
+  }
 
-  // Calculate result
+  // calculate result
   let result = (target / base) * amount;
-  // Build output HTML
-  let output = `<p class="center-align">
-                Adding <strong>${result}mL</strong> of your nicotine base to ${amount}mL of eLiquid will achieve your target strength of ${target}mg.
-                </p>
-               `;
-  // Return output
-  return (outputArea.innerHTML = output);
+  // round to 2 decimal places
+  result = result.toFixed(2);
+
+  // return result
+  return `<p class="center-align">
+          Adding <strong>${result}mL</strong> of your nicotine base 
+          to ${amount}mL of eLiquid <br> will achieve your target strength of ${target}mg.
+          </p>
+          `;
 }
