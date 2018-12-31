135db0a64e3954721fc25f61665e21840ca13270
Jim Farrugia <the.jim.farrugia@gmail.com>
2018-12-31 21:32:17 +1100

    Cleaned up output and error display.

--- app.js
+++ app.js
@@ -14,22 +14,26 @@ function calculateNicToAdd(base, target, amount) {
   // Check for invalid inputs
   if (!base || !target || !amount) {
     // values are not truthy
-    return `<p>One or more input is invalid.</p>`;
+    return `<p class="error">One or more input is invalid.</p>`;
   }
   if (base == 0 || target == 0 || amount == 0) {
     // values cannot be 0
-    return `<p>All inputs must be grater than zero.</p>`;
+    return `<p class="error">All inputs must be grater than zero.</p>`;
   }
 
   // calculate result
   let result = (target / base) * amount;
+  // convert to grams
+  weight = result * 1.038;
   // round to 2 decimal places
   result = result.toFixed(2);
+  weight = weight.toFixed(2);
 
   // return result
-  return `<p class="center-align">
-          Adding <strong>${result}mL</strong> of your nicotine base 
-          to ${amount}mL of eLiquid <br> will achieve your target strength of ${target}mg.
+  return `<p>
+            Your ${amount}mL batch should contain <strong>${result}mL</strong> of your nicotine base in order to achieve your target strength of ${target}mg.
+            <br/><br/>
+            <em>${result}mL of nicotine liquid weighs approximately <strong>${weight} grams</strong>.</em>
           </p>
           `;
 }
