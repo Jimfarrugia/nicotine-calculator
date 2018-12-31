f1ddb8ea82e6b0a3ccbcaf6d2399669ad8723a5b
Jim Farrugia <the.jim.farrugia@gmail.com>
2018-12-31 18:30:59 +1100

    Added output to script.

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ document.getElementById("calculate-btn").addEventListener("click", function() {
   const baseInput = document.getElementById("base-input").value;
   const targetInput = document.getElementById("target-input").value;
   const amountInput = document.getElementById("amount-input").value;
+  const outputArea = document.getElementById("result");
   // Call calculate function
-  calculateNicToAdd(baseInput, targetInput, amountInput);
+  calculateNicToAdd(baseInput, targetInput, amountInput, outputArea);
+  // Display result
 });
 
 // CALCULATE
-function calculateNicToAdd(base, target, amount) {
-  //
-  return console.log(base, target, amount);
+function calculateNicToAdd(base, target, amount, outputArea) {
+  // perform calculation and return result as html to user
+  let output = "test";
+
+  return (outputArea.innerHTML = output);
 }
